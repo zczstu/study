9374060313196c46344160545093b024cb210d69
ZCZ <lutation@126.com>
2017-07-25 08:57:18 +0800

    addstudy1

--- maven_study/src/main/java/study1.java
+++ maven_study/src/main/java/study1.java
@@ -1,7 +1,23 @@
-package PACKAGE_NAME;
+import org.jsoup.Jsoup;
 
 /**
  * Created by jojo on 2017/7/24.
  */
 public class study1 {
+
+    public static void main(String[] args){
+        System.out.println(Tools.sayHelloToPerson("zhang"));
+        System.out.println("hahahaha");
+        System.out.println("hello");
+
+    }
+}
+abstract class Tools
+{
+
+    public static String sayHelloToPerson(String name)
+    {
+        return String.format("hello world,from %s",name);
+    }
+    abstract void process();
 }
