3041e7c9e42cfd34803e8201cd8b8cbd53e6e7d2
ZCZ <lutation@126.com>
2017-07-29 12:30:06 +0800

    file study 1

--- maven_study/src/main/java/file1.java
+++ maven_study/src/main/java/file1.java
@@ -9,19 +9,86 @@ import java.io.FileReader;
 import java.io.RandomAccessFile;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.UUID;
 
 public class file1 {
-    public static void main(String[] args) {/*
-        try{
-            String data = "aaaaaaaaaa";
-            File fileweb = new File("e:/gitstudy/web.txt");
-            if(!fileweb.exists()){
-                fileweb.createNewFile();
-            }*/
-
-
-
-
+    public static boolean filecreate(String filename, String filecontent) {
+        boolean bool = false;
+        File file = new File(filename);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+                bool = true;
+                System.out.println("create success");
+                //write to file
+                writetofile(filename, filecontent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bool;
     }
 
+    public static boolean writetofile(String filename, String newcontent)throws IOException {
+        boolean bool = false;
+        String filein = newcontent+"\r\n";
+        String temp  = "";
+
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        FileOutputStream fos  = null;
+        PrintWriter pw = null;
+        try {
+            File file = new File(filename);//文件路径(包括文件名称)
+            //将文件读入输入流
+            fis = new FileInputStream(file);
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            StringBuffer buffer = new StringBuffer();
+
+            //文件原有内容
+            for(int i=0;(temp =br.readLine())!=null;i++){
+                buffer.append(temp);
+                // 行与行之间的分隔符 相当于“\n”
+                buffer = buffer.append(System.getProperty("line.separator"));
+            }
+            buffer.append(filein);
+
+            fos = new FileOutputStream(file);
+            pw = new PrintWriter(fos);
+            pw.write(buffer.toString().toCharArray());
+            pw.flush();
+            bool = true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }finally {
+            if (pw != null) {
+                pw.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+            if (isr != null) {
+                isr.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+        }
+        return bool;
+    }
+
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        filecreate("e:\\gitstudy\\file1.txt","create a file");
+
+    }
 }
